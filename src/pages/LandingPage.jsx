db33d31076d24d9b6218cff7e82ee4113c51225f
jayant-vashisth <jayant.relish@gmail.com>
2024-01-13 23:16:36 +0530

    project setup

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const LandingPage = () => {
+  return (
+    <div className="App-header">
+      <h1 className="text-3xl font-bold underline border text-blue-500">Hello world!</h1>
+    </div>
+  );
+};
